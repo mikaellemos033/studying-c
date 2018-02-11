1ba8f03beb39789014c1d73e8c2c1fc7d9ed9b02
mikaellemos033 <mikaellemos033@gmail.com>
2018-02-11 13:12:57 -0200

    incluinde simples de arquivos incluido dentro de outro

---
+++ functions/require/func.c
@@ -0,0 +1,3 @@
+int decima() {
+	return 10;
+}
---
+++ functions/require/master.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "func.c"
+
+int main (void) {
+
+	printf("%d it is a number of decima \n", decima());
+
+	return 0;
+}
